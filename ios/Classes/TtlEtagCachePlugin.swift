7ac4c7528915fd593a440aeda27b54c580088a96
loicgeek <loic.ngou98@gmail.com>
2025-11-25 15:57:37 -0500

    added ios

--- ios/Classes/TtlEtagCachePlugin.swift
+++ ios/Classes/TtlEtagCachePlugin.swift
@@ -3,17 +3,15 @@ import UIKit
 
 public class TtlEtagCachePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "ttl_etag_cache", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(
+      name: "ttl_etag_cache", 
+      binaryMessenger: registrar.messenger()
+    )
     let instance = TtlEtagCachePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
-    }
+    result(FlutterMethodNotImplemented)
   }
 }
